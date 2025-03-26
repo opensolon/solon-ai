7542be9410e220336d1841f185e81a6a89a1f322
noear <noear@live.cn>
2025-03-26 20:56:49 +0800

    调整 solon-ai-load-excel 单测包名

--- solon-ai-projects/solon-ai-load-excel/src/test/java/features/ai/load/excel/ExcelLoaderTest.java
+++ solon-ai-projects/solon-ai-load-excel/src/test/java/features/ai/load/excel/ExcelLoaderTest.java
@@ -1,12 +1,13 @@
-package org.noear.solon.ai.rag.loader;
-
-import java.util.List;
+package features.ai.load.excel;
 
 import org.junit.jupiter.api.Test;
 import org.noear.solon.ai.rag.Document;
+import org.noear.solon.ai.rag.loader.ExcelLoader;
 import org.noear.solon.core.util.ResourceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class ExcelLoaderTest {
     private static final Logger log = LoggerFactory.getLogger(ExcelLoaderTest.class);
