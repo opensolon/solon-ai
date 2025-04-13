0dba2f1f2998dcd7ed9f3d6a568ed492779888e2
noear <noear@live.cn>
2025-04-13 15:34:20 +0800

    简化 solon-ai-mcp 测试试例

--- solon-ai-mcp/src/test/java/demo/ai/mcp/client/McpClientConfig.java
+++ solon-ai-mcp/src/test/java/demo/ai/mcp/client/McpClientConfig.java
@@ -1,6 +1,5 @@
 package demo.ai.mcp.client;
 
-import org.noear.solon.ai.chat.ChatConfig;
 import org.noear.solon.ai.chat.ChatModel;
 import org.noear.solon.ai.mcp.client.McpClientToolProvider;
 import org.noear.solon.annotation.Bean;
@@ -9,13 +8,9 @@ import org.noear.solon.annotation.Inject;
 
 @Configuration
 public class McpClientConfig {
-    private static final String apiUrl = "http://127.0.0.1:11434/api/chat";
-    private static final String provider = "ollama";
-    private static final String model = "qwen2.5:1.5b"; //"llama3.2";//deepseek-r1:1.5b;
-
     @Bean
-    public ChatModel chatModel(@Inject("${solon.ai.chat.demo}") ChatConfig config) {
-        return ChatModel.of(config).build();
+    public ChatModel chatModel(@Inject("${solon.ai.chat.demo}") ChatModel chatModel) {
+        return chatModel;
     }
 
     @Bean
