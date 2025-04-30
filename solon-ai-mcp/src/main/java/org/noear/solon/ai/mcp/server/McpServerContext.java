9621cba41d1e452fa353e86704a837672aca2217
noear <noear@live.cn>
2025-04-30 22:24:02 +0800

    添加 solon-ai-mcp McpServerContext sessionState 和 cookie 支持

--- solon-ai-mcp/src/main/java/org/noear/solon/ai/mcp/server/McpServerContext.java
+++ solon-ai-mcp/src/main/java/org/noear/solon/ai/mcp/server/McpServerContext.java
@@ -4,6 +4,7 @@ import io.modelcontextprotocol.server.McpSyncServerExchange;
 import io.modelcontextprotocol.server.transport.WebRxSseServerTransportProvider;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.ContextEmpty;
+import org.noear.solon.core.handle.SessionState;
 import org.noear.solon.core.util.MultiMap;
 
 import java.util.Collection;
@@ -32,25 +33,90 @@ public class McpServerContext extends ContextEmpty {
         return serverExchange.getSession().getId();
     }
 
-    /**
-     * 获取会话 header
-     */
+    /// ////////////////
+
+    @Override
+    public SessionState sessionState() {
+        return context.sessionState();
+    }
+
+    @Override
+    public <T> T session(String name, Class<T> clz) {
+        return context.session(name, clz);
+    }
+
+    @Override
+    public double sessionAsDouble(String name) {
+        return context.sessionAsDouble(name);
+    }
+
+    @Override
+    public double sessionAsDouble(String name, double def) {
+        return context.sessionAsDouble(name, def);
+    }
+
+    @Override
+    public int sessionAsInt(String name) {
+        return context.sessionAsInt(name);
+    }
+
+    @Override
+    public int sessionAsInt(String name, int def) {
+        return context.sessionAsInt(name, def);
+    }
+
+    @Override
+    public long sessionAsLong(String name) {
+        return context.sessionAsLong(name);
+    }
+
+    @Override
+    public long sessionAsLong(String name, long def) {
+        return context.sessionAsLong(name, def);
+    }
+
+    @Override
+    public <T> T sessionOrDefault(String name, T def) {
+        return context.sessionOrDefault(name, def);
+    }
+
+    @Override
+    public void sessionSet(String name, Object val) {
+        context.sessionSet(name, val);
+    }
+
+    @Override
+    public void sessionClear() {
+        context.sessionClear();
+    }
+
+    @Override
+    public void sessionRemove(String name) {
+        context.sessionRemove(name);
+    }
+
+    @Override
+    public void sessionReset() {
+        context.sessionReset();
+    }
+
+    /// /////////////////
+
     @Override
     public String header(String name) {
         return context.header(name);
     }
 
-    /**
-     * 获取会话 header
-     */
+    @Override
+    public String headerOrDefault(String name, String def) {
+        return context.headerOrDefault(name, def);
+    }
+
     @Override
     public String[] headerValues(String name) {
         return context.headerValues(name);
     }
 
-    /**
-     * 获取会话 headerNames
-     */
     @Override
     public Collection<String> headerNames() {
         return context.headerNames();
@@ -58,6 +124,33 @@ public class McpServerContext extends ContextEmpty {
 
     @Override
     public MultiMap<String> headerMap() {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    @Override
+    public String cookie(String name) {
+        return context.cookie(name);
+    }
+
+    @Override
+    public String cookieOrDefault(String name, String def) {
+        return context.cookieOrDefault(name, def);
+    }
+
+    @Override
+    public String[] cookieValues(String name) {
+        return context.cookieValues(name);
+    }
+
+    @Override
+    public Collection<String> cookieNames() {
+        return context.cookieNames();
+    }
+
+    @Override
+    public MultiMap<String> cookieMap() {
         throw new UnsupportedOperationException();
     }
 }
