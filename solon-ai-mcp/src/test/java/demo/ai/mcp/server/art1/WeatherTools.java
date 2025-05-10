adab8dd15bda7d136ba9124c5655747899d1b75d
noear <noear@live.cn>
2025-05-11 00:43:11 +0800

    优化 solon-ai-mcp 取消 MethodFunctionResource 对反回类型的限制（增加了 resultConverter 转换处理）

--- solon-ai-mcp/src/test/java/demo/ai/mcp/server/art1/WeatherTools.java
+++ solon-ai-mcp/src/test/java/demo/ai/mcp/server/art1/WeatherTools.java
@@ -7,6 +7,9 @@ import org.noear.solon.annotation.Param;
 import org.noear.solon.annotation.Produces;
 import org.noear.solon.core.util.MimeType;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @McpServerEndpoint(sseEndpoint = "/mcp/WeatherTools/sse")
 public class WeatherTools {
@@ -15,14 +18,15 @@ public class WeatherTools {
         return "{city: '" + city + "', temperature:[10,25], condition:['sunny', 'clear', 'hot'], unit:celsius}";
     }
 
+    //给前端用，需要严格的 json 格式
     @Produces(MimeType.APPLICATION_JSON_VALUE)
     @ResourceMapping(uri = "weather://cities", description = "获取所有可用的城市列表")
-    public String[] get_available_cities() {
-        return new String[]{"Tokyo", "Sydney", "Tokyo"};
+    public List<String> get_available_cities() {
+        return Arrays.asList("Tokyo", "Sydney", "Tokyo");
     }
 
     @ResourceMapping(uri = "weather://forecast/{city}", description = "获取指定城市的天气预报资源")
     public String get_forecast(@Param String city) {
-        return "{city: " + city + ", forecast: 111}";
+        return "{city: " + city + ", forecast: [{temperature:[10,25], condition:['sunny', 'clear', 'hot']}]}";
     }
 }
