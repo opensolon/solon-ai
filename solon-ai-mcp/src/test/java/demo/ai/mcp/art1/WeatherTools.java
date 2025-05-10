4f3e7d155458ef0b363723125761529f527b4604
noear <noear@live.cn>
2025-05-10 21:21:12 +0800

    添加新的示例

--- solon-ai-mcp/src/test/java/demo/ai/mcp/art1/WeatherTools.java
+++ solon-ai-mcp/src/test/java/demo/ai/mcp/art1/WeatherTools.java
@@ -9,27 +9,13 @@ import org.noear.solon.annotation.Param;
 
 @McpServerEndpoint(channel = McpChannel.STDIO)
 public class WeatherTools {
-    ONode weather_data = ONode.loadStr("{\n" +
-            "    \"New York\": {\"temp\": range(10, 25), \"conditions\": [\"sunny\", \"cloudy\", \"rainy\"]},\n" +
-            "    \"London\": {\"temp\": range(5, 20), \"conditions\": [\"cloudy\", \"rainy\", \"foggy\"]},\n" +
-            "    \"Tokyo\": {\"temp\": range(15, 30), \"conditions\": [\"sunny\", \"cloudy\", \"humid\"]},\n" +
-            "    \"Sydney\": {\"temp\": range(20, 35), \"conditions\": [\"sunny\", \"clear\", \"hot\"]},\n" +
-            "}");
-
     @ToolMapping(description = "获取指定城市的当前天气")
-    public ONode get_weather(@Param String city) {
-        if (weather_data.contains(city) == false) {
-            return new ONode().set("error", "无法找到城市 " + city + " 的天气数据");
-        }
-
-        //ONode tmp = weather_data.get(city);
-        //return "24度，晴";
-
-        return null;
+    public String get_weather(@Param String city) {
+        return "24度，晴";
     }
 
     @ResourceMapping(uri = "weather://forecast/{city}")
-    public String get_available_cities(String city){
-        return "{city: "+city+", forecast: 111}";
+    public String get_available_cities(String city) {
+        return "{city: " + city + ", forecast: 111}";
     }
 }
