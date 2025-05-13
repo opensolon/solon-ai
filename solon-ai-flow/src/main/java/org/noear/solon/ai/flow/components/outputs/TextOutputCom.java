050b0135f05222d9d740d10e963e38ce5e01569f
noear <noear@live.cn>
2025-05-13 16:35:47 +0800

    优化 solon-ai-flow TextOutputCom 接口设计，支持循环输出（支持两智能体对话的效果）

--- solon-ai-flow/src/main/java/org/noear/solon/ai/flow/components/outputs/TextOutputCom.java
+++ solon-ai-flow/src/main/java/org/noear/solon/ai/flow/components/outputs/TextOutputCom.java
@@ -9,7 +9,7 @@ import org.noear.solon.flow.Node;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
-import java.io.PrintStream;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * 文本输出组件
@@ -19,32 +19,42 @@ import java.io.PrintStream;
  */
 @Component("TextOutput")
 public class TextOutputCom extends AbsAiComponent implements AiIoComponent {
-    @Override
-    public void setOutput(FlowContext context, Node node, Object data) throws Throwable {
-        final PrintStream out = System.out;
-
-        if (data instanceof Publisher) {
-            Flux.from((Publisher<ChatResponse>) data)
-                    .filter(resp -> resp.hasChoices())
-                    .map(resp -> resp.getMessage())
-                    .doOnNext(msg -> {
-                        out.println(msg);
-                    })
-                    .subscribe();
-
-        } else if (data instanceof ChatResponse) {
-            ChatResponse resp = (ChatResponse) data;
-            data = resp.getMessage();
-
-            out.println(data);
-        } else {
-            out.println(data);
-        }
-    }
 
     @Override
     protected void doRun(FlowContext context, Node node) throws Throwable {
         Object data = getInput(context, node);
+
+        final StringBuilder buf = new StringBuilder();
+
+        if (data instanceof Publisher) {
+            CountDownLatch latch = new CountDownLatch(1);
+            Flux.from((Publisher<ChatResponse>) data)
+                    .filter(resp -> resp.hasChoices())
+                    .map(resp -> resp.getMessage())
+                    .doOnNext(msg -> {
+                        buf.append(msg.getContent());
+                    })
+                    .doOnComplete(() -> {
+                        latch.countDown();
+                    })
+                    .doOnError(err -> {
+                        err.printStackTrace();
+                        latch.countDown();
+                    })
+                    .subscribe();
+
+            latch.await();
+        } else if (data instanceof ChatResponse) {
+            ChatResponse resp = (ChatResponse) data;
+            data = resp.getMessage().getContent();
+
+            buf.append(data);
+        } else {
+            buf.append(data);
+        }
+
+        data = buf.toString();
+        System.out.println(data);
 
         setOutput(context, node, data);
     }
