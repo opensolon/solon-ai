a9272c55591e5d1e7983564efb0b5ff8c0ef5890
noear <noear@live.cn>
2025-05-19 01:01:56 +0800

    优化 solon-ai-flow 示例

--- solon-ai-flow/src/test/java/features/ai/flow/app/DemoController.java
+++ solon-ai-flow/src/test/java/features/ai/flow/app/DemoController.java
@@ -8,7 +8,6 @@ import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.annotation.Produces;
-import org.noear.solon.core.handle.UploadedFile;
 import org.noear.solon.core.util.MimeType;
 import org.noear.solon.flow.FlowContext;
 import org.noear.solon.flow.stateful.StatefulFlowEngine;
@@ -25,11 +24,8 @@ public class DemoController {
 
     @Produces(MimeType.TEXT_EVENT_STREAM_VALUE)
     @Mapping("chat_case2")
-    public Object chat_case2(String message, UploadedFile attachment) throws Exception {
+    public void chat_case2() throws Exception {
         FlowContext flowContext = new FlowContext();
-
-        flowContext.put(Attrs.CTX_MESSAGE, message);
-        flowContext.put(Attrs.CTX_ATTACHMENT, attachment);
 
         //保存会话记录
         ChatSession chatSession = chatSessionMap.computeIfAbsent("chat_case2", k -> new ChatSessionDefault());
@@ -41,33 +37,15 @@ public class DemoController {
         });
 
         flowEngine.eval("chat_case2", flowContext);
-
-        return flowContext.get(Attrs.CTX_MESSAGE);
     }
 
-    @Produces(MimeType.TEXT_EVENT_STREAM_VALUE)
     @Mapping("rag_case2")
-    public Object rag_case2(String message, UploadedFile attachment) throws Exception {
-        FlowContext flowContext = new FlowContext();
-
-        flowContext.put(Attrs.CTX_MESSAGE, message);
-        flowContext.put(Attrs.CTX_ATTACHMENT, attachment);
-
-        flowEngine.eval("rag_case2", flowContext);
-
-        return flowContext.get(Attrs.CTX_MESSAGE);
+    public void rag_case2() throws Exception {
+        flowEngine.eval("rag_case2");
     }
 
-    @Produces(MimeType.TEXT_EVENT_STREAM_VALUE)
     @Mapping("tool_case2")
-    public Object tool_case2(String message, UploadedFile attachment) throws Exception {
-        FlowContext flowContext = new FlowContext();
-
-        flowContext.put(Attrs.CTX_MESSAGE, message);
-        flowContext.put(Attrs.CTX_ATTACHMENT, attachment);
-
-        flowEngine.eval("tool_case2", flowContext);
-
-        return flowContext.get(Attrs.CTX_MESSAGE);
+    public void tool_case2() throws Exception {
+        flowEngine.eval("tool_case2");
     }
 }
