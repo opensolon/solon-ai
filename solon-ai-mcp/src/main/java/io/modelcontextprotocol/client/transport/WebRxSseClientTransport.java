1951995af5b553096f054c6cc831e6ad0345336f
noear <noear@live.cn>
2025-07-15 14:32:39 +0800

    mcp WebRxSseClientTransport 添加 debug 日志打印

--- solon-ai-mcp/src/main/java/io/modelcontextprotocol/client/transport/WebRxSseClientTransport.java
+++ solon-ai-mcp/src/main/java/io/modelcontextprotocol/client/transport/WebRxSseClientTransport.java
@@ -212,7 +212,7 @@ public class WebRxSseClientTransport implements McpClientTransport {
 
 					try {
 						if(logger.isDebugEnabled()) {
-							logger.debug("mcp-response: {}",event.toString());
+							logger.debug("Mcp-response: {}",event.toString());
 						}
 
 						if (ENDPOINT_EVENT_TYPE.equals(event.getEvent())) {
@@ -280,7 +280,7 @@ public class WebRxSseClientTransport implements McpClientTransport {
 			String jsonText = this.objectMapper.writeValueAsString(message);
 
 			if (logger.isDebugEnabled()) {
-				logger.debug("mcp-request: {}", jsonText);
+				logger.debug("Mcp-request: {}", jsonText);
 			}
 
 			CompletableFuture<HttpResponse> future = webBuilder.build(endpoint)
