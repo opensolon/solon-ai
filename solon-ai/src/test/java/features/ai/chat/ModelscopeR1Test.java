25f7cbd8fb0424ee6d99c082b02d4e7bb6aa218f
noear <noear@live.cn>
2025-07-24 21:00:14 +0800

    添加 solon-ai 新单测

---
+++ solon-ai/src/test/java/features/ai/chat/ModelscopeR1Test.java
@@ -0,0 +1,22 @@
+package features.ai.chat;
+
+import features.ai.chat.interceptor.ChatInterceptorTest;
+import org.noear.solon.ai.chat.ChatModel;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2025/1/28 created
+ */
+@SolonTest
+public class ModelscopeR1Test extends AbsChatTest{
+    private static final String apiUrl = "https://api-inference.modelscope.cn/v1/chat/completions";
+    private static final String apiKey = "a90656bf-08b2-47c8-b791-f5be78fe15de";
+    private static final String model = "deepseek-ai/DeepSeek-R1-0528"; //"Qwen/Qwen3-32B";
+
+    protected ChatModel.Builder getChatModelBuilder() {
+        return ChatModel.of(apiUrl)
+                .apiKey(apiKey)
+                .model(model)
+                .defaultInterceptorAdd(new ChatInterceptorTest());
+    }
+}
