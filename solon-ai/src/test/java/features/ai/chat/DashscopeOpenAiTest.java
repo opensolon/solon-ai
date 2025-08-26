f2bcba7bec4f5d33cc8a5143b7d6fe0d4a3bad0a
noear <noear@live.cn>
2025-08-26 16:38:23 +0800

    添加单测 DashscopeOpenAiTest

---
+++ solon-ai/src/test/java/features/ai/chat/DashscopeOpenAiTest.java
@@ -0,0 +1,20 @@
+package features.ai.chat;
+
+import org.noear.solon.ai.chat.ChatModel;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2025/1/28 created
+ */
+@SolonTest
+public class DashscopeOpenAiTest extends AbsChatTest{
+    private static final String apiUrl = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions";
+    private static final String apiKey = "sk-1ffe449611a74e61ad8e71e1b35a9858";
+    private static final String model = "qwen-turbo-latest";//"llama3.2"; //deepseek-r1:1.5b;
+
+    protected ChatModel.Builder getChatModelBuilder() {
+        return ChatModel.of(apiUrl)
+                .apiKey(apiKey)
+                .model(model);
+    }
+}
