8a9942bc5a2540ac310f4feb07f91fc922f32a23
noear <noear@live.cn>
2025-09-30 17:57:49 +0800

    修复 solon-ai-mcp WebRxSseClientTransport:sendMessage 使用 okhttp 适配时可能会出错的问题（原因：没有显式申明 accept）

--- solon-ai-mcp/src/test/java/lab/ai/mcp/client/ModelscopeTest.java
+++ solon-ai-mcp/src/test/java/lab/ai/mcp/client/ModelscopeTest.java
@@ -2,6 +2,7 @@ package lab.ai.mcp.client;
 
 import org.junit.jupiter.api.Test;
 import org.noear.solon.ai.chat.tool.FunctionTool;
+import org.noear.solon.ai.mcp.McpChannel;
 import org.noear.solon.ai.mcp.client.McpClientProvider;
 
 import java.util.Collection;
@@ -14,7 +15,25 @@ public class ModelscopeTest {
     public void case1() {
         //https://modelscope.cn/mcp/servers/@antvis/mcp-server-chart
         McpClientProvider clientProvider = McpClientProvider.builder()
-                .apiUrl("https://mcp.api-inference.modelscope.net/381da551144140/sse")
+                .channel(McpChannel.SSE)
+                .apiUrl("https://mcp.api-inference.modelscope.net/9059e0e1da5743/sse")
+                .build();
+
+        Collection<FunctionTool> tools = clientProvider.getTools();
+
+        System.out.println("------------------------------------------------");
+        System.out.println(tools);
+
+        assert tools != null;
+        assert tools.size() > 1;
+    }
+
+    @Test
+    public void case2() {
+        //https://modelscope.cn/mcp/servers/chevalblanc/MCP-BING-CN
+        McpClientProvider clientProvider = McpClientProvider.builder()
+                .channel(McpChannel.STREAMABLE)
+                .apiUrl("https://mcp.api-inference.modelscope.net/a707ccd11ea647/mcp")
                 .build();
 
         Collection<FunctionTool> tools = clientProvider.getTools();
