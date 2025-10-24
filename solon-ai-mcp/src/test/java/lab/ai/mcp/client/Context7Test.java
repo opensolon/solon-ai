d5f726a289b62958e52d4b5778fcdbf59aff8102
noear <noear@live.cn>
2025-10-24 13:22:49 +0800

    添加新测试

--- solon-ai-mcp/src/test/java/lab/ai/mcp/client/Context7Test.java
+++ solon-ai-mcp/src/test/java/lab/ai/mcp/client/Context7Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.noear.solon.ai.chat.tool.FunctionTool;
 import org.noear.solon.ai.mcp.McpChannel;
 import org.noear.solon.ai.mcp.client.McpClientProvider;
+import org.noear.solon.net.http.HttpTimeout;
 
 import java.util.Collection;
 
@@ -12,9 +13,10 @@ import java.util.Collection;
  */
 public class Context7Test {
     @Test
-    public void case1() {
+    public void case1() throws Exception {
         McpClientProvider clientProvider = McpClientProvider.builder()
                 .channel(McpChannel.STREAMABLE)
+                .httpTimeout(HttpTimeout.of(30, 30, 0))
                 .apiUrl("https://mcp.context7.com/mcp")
                 .header("CONTEXT7_API_KEY", "xxx")
                 .build();
@@ -26,5 +28,7 @@ public class Context7Test {
 
         assert tools != null;
         assert tools.size() > 1;
+
+        Thread.sleep(1000 * 60 * 5);
     }
 }
