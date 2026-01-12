2514ac169a73073edfea343a0a27d0342b6172f3
noear <noear@live.cn>
2026-01-12 18:30:03 +0800

    调整 solon-ai-dialect-gemini java 包名（...openai 改为 ...gemini）

--- solon-ai-llm-dialects/solon-ai-dialect-gemini/src/main/java/org/noear/solon/ai/llm/dialect/gemini/GeminiChatDialect.java
+++ solon-ai-llm-dialects/solon-ai-dialect-gemini/src/main/java/org/noear/solon/ai/llm/dialect/gemini/GeminiChatDialect.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.noear.solon.ai.llm.dialect.openai;
+package org.noear.solon.ai.llm.dialect.gemini;
 
 import org.noear.snack4.ONode;
 import org.noear.solon.Utils;
