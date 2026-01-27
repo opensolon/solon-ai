f7c392e1e82c28eee6f4e2e8b1ee7da9bee6daa5
noear <noear@live.cn>
2026-01-27 14:03:34 +0800

    添加 ClaudeTest

---
+++ solon-ai/src/test/java/features/ai/chat/ClaudeTest.java
@@ -0,0 +1,17 @@
+package features.ai.chat;
+
+import org.noear.solon.ai.chat.ChatModel;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2025/1/28 created
+ */
+@SolonTest
+public class ClaudeTest extends AbsChatTest{
+    protected ChatModel.Builder getChatModelBuilder() {
+        return ChatModel.of("")
+                .apiKey("")
+                .provider("claude")
+                .model("");
+    }
+}
