094ae43b81de4b7902d950d8a9b84d2a218c2b82
noear <noear@live.cn>
2026-01-30 22:56:51 +0800

    添另 FileStorageSkill

--- solon-ai-skills/solon-ai-skill-basics/src/test/java/demo/ai/skill/web/Demo.java
+++ solon-ai-skills/solon-ai-skill-basics/src/test/java/demo/ai/skill/web/Demo.java
@@ -1,6 +1,8 @@
 package demo.ai.skill.web;
 
 import org.noear.solon.ai.chat.ChatModel;
+import org.noear.solon.ai.skills.file.FileStorageSkill;
+import org.noear.solon.ai.skills.system.SystemClockSkill;
 import org.noear.solon.ai.skills.web.WebCrawlerSkill;
 import org.noear.solon.ai.skills.web.WebSearchSkill;
 
@@ -13,19 +15,14 @@ public class Demo {
     public void test() throws Exception {
         WebSearchSkill search = new WebSearchSkill(WebSearchSkill.SERPER, "serper_key");
         WebCrawlerSkill crawler = new WebCrawlerSkill(WebCrawlerSkill.FIRECRAWL, "jina_key");
+        FileStorageSkill disk = new FileStorageSkill("./ai_workspace");
+        SystemClockSkill clock = new SystemClockSkill();
 
         ChatModel agent = ChatModel.of("url..")
                 .model("xxx")
-                .defaultSkillAdd(search)
-                .defaultSkillAdd(crawler)
+                .defaultSkillAdd(clock, disk, search, crawler)
                 .build();
 
-        // 对话逻辑：
-        // 1. AI 意识到不知道“Solon AOT”是什么
-        // 2. AI 调用 web_search("Solon AOT 机制原理")
-        // 3. AI 拿到几个 Link，发现第一个官网 Link 很关键
-        // 4. AI 调用 web_crawler("https://solon.noear.org/...") 深度阅读文档
-        // 5. AI 给用户详细解答
-        agent.prompt("详细解释一下 Solon 的 AOT 编译机制原理").call();
-    }
+        agent.prompt("搜索 2026 年最流行的 Java 框架，选择前三名抓取正文，并分别保存为 markdown 文件。")
+                .call();    }
 }
