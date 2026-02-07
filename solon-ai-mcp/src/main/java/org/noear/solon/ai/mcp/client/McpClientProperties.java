0997e915a1c8b3cd8f42fb2510d2df0bd3c5fa05
noear <noear@live.cn>
2026-02-07 23:05:16 +0800

    调整 solon-ai-mcp 默认不启用心跳

--- solon-ai-mcp/src/main/java/org/noear/solon/ai/mcp/client/McpClientProperties.java
+++ solon-ai-mcp/src/main/java/org/noear/solon/ai/mcp/client/McpClientProperties.java
@@ -121,9 +121,9 @@ public class McpClientProperties {
 
 
     /**
-     * mcp 心跳间隔（辅助自动重连）
+     * mcp 心跳间隔（辅助自动重连，默认不启用）
      */
-    private Duration heartbeatInterval = Duration.ofSeconds(15);
+    private Duration heartbeatInterval = null;
 
     /**
      * mcp 缓存秒数
