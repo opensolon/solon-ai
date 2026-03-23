f48f147964a69725f807417a76f9757d2c190603
noear <noear@live.cn>
2026-03-23 13:30:12 +0800

    优化 solon-ai-skill-memory MemoryStoreProviderRogueImpl

--- solon-ai-skills/solon-ai-skill-memory/src/main/java/org/noear/solon/ai/skills/memory/store/MemoryStoreProviderRogueImpl.java
+++ solon-ai-skills/solon-ai-skill-memory/src/main/java/org/noear/solon/ai/skills/memory/store/MemoryStoreProviderRogueImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2026 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.noear.solon.ai.skills.memory.store;
 
 import com.yomahub.roguemap.RogueMap;
@@ -15,9 +30,9 @@ import java.util.concurrent.TimeUnit;
 public class MemoryStoreProviderRogueImpl implements MemoryStoreProvider {
     private final RogueMap<String, String> rogueMap;
 
-    public MemoryStoreProviderRogueImpl(String path) {
+    public MemoryStoreProviderRogueImpl(String filePath) {
         this.rogueMap = RogueMap.<String, String>mmap()
-                .persistent(Paths.get(path, "memory/rogue.db").toAbsolutePath().toString())
+                .persistent(filePath)
                 .autoExpand(true)
                 .allocateSize(64 * 1024 * 1024L)
                 .keyCodec(StringCodec.INSTANCE)
