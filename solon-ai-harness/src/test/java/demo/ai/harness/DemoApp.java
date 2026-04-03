39999fe7a4d21ba59e5ecd5035df65af501f16ba
noear <noear@live.cn>
2026-04-03 13:52:28 +0800

    3.10.1

--- solon-ai-harness/src/test/java/demo/ai/harness/DemoApp.java
+++ solon-ai-harness/src/test/java/demo/ai/harness/DemoApp.java
@@ -2,38 +2,76 @@ package demo.ai.harness;
 
 import org.noear.solon.ai.agent.AgentSession;
 import org.noear.solon.ai.agent.AgentSessionProvider;
-import org.noear.solon.ai.agent.session.FileAgentSession;
+import org.noear.solon.ai.agent.react.ReActAgent;
+import org.noear.solon.ai.agent.session.InMemoryAgentSession;
 import org.noear.solon.ai.chat.ChatModel;
 import org.noear.solon.ai.harness.HarnessEngine;
 import org.noear.solon.ai.harness.HarnessProperties;
+import org.noear.solon.ai.harness.agent.AgentDefinition;
 import org.noear.solon.ai.harness.permission.ToolPermission;
 
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-/**
- *
- * @author noear 2026/4/3 created
- *
- */
 public class DemoApp {
-    public static void main(String[] arg) {
-        HarnessProperties agentProps = new HarnessProperties(".solon/");
+    public static void main(String[] arg) throws Throwable {
+        //--- 1. 初始化
+        HarnessProperties harnessProps = new HarnessProperties(".tmp/");
+        harnessProps.addTools(ToolPermission.TOOL_ALL_FULL); //设定工具权限
+        harnessProps.setChatModel(null); //设定大模型配置
 
-        agentProps.addTools(ToolPermission.TOOL_ALL_FULL);
+        ChatModel chatModel = ChatModel.of(harnessProps.getChatModel()).build();
+        AgentSessionProvider sessionProvider = new AgentSessionProvider() {
+            private Map<String, AgentSession> sessionMap = new ConcurrentHashMap<>();
 
-        ChatModel chatModel = ChatModel.of(agentProps.getChatModel()).build();
-        Map<String, AgentSession> sessionMap = new ConcurrentHashMap<>();
+            @Override
+            public AgentSession getSession(String instanceId) {
+                return sessionMap.computeIfAbsent(instanceId, k -> InMemoryAgentSession.of(k));
+            }
+        };
 
-        AgentSessionProvider sessionProvider = (sessionId) -> sessionMap.computeIfAbsent(sessionId, key ->
-                new FileAgentSession(key, Paths.get(agentProps.getWorkspace(), agentProps.getHarnessSessions())
-                        .resolve(key).normalize().toFile().toString()));
-
-        HarnessEngine agentRuntime = HarnessEngine.builder()
+        HarnessEngine engine = HarnessEngine.builder()
+                .properties(harnessProps)
                 .chatModel(chatModel)
-                .properties(agentProps)
                 .sessionProvider(sessionProvider)
                 .build();
+
+        //--- 用主代理执行
+        csae1(engine, "hello");
+
+        //--- 动态创建子代理执行（好处理可以动态创建不同的工具权限）
+        case2(engine, "hello");
+
+    }
+
+    private static void csae1(HarnessEngine engine, String prompt) throws Throwable {
+        AgentSession session = engine.getSession(HarnessEngine.SESSION_DEFAULT);
+
+        //--- 用主代理模式
+        engine.getMainAgent().prompt(prompt)
+                .session(session) //没有，则为临时会话
+                .options(o -> {
+                    //按需，动态指定工作区（没有，则为默认工作区）
+                    o.toolContextPut(HarnessEngine.ATTR_CWD, "xxx");
+                })
+                .call();
+    }
+
+    private static void case2(HarnessEngine engine, String prompt) throws Throwable {
+        AgentSession session = engine.getSession(HarnessEngine.SESSION_DEFAULT);
+
+        //动态定义智能体
+        AgentDefinition definition = new AgentDefinition();
+        definition.setSystemPrompt("xxx"); //系统提示词
+        definition.getMetadata().addTools(ToolPermission.TOOL_BASH); //工具权限
+
+        ReActAgent subagent = engine.createSubagent(definition).build();
+        subagent.prompt(prompt)
+                .session(session) //没有，则为临时会话
+                .options(o -> {
+                    //按需，动态指定工作区（没有，则为默认工作区）
+                    o.toolContextPut(HarnessEngine.ATTR_CWD, "xxx");
+                })
+                .call();
     }
 }
