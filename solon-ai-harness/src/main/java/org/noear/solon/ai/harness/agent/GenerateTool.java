db727acb0f5c519e1b33a7ce356176e7b64c03b1
noear <noear@live.cn>
2026-04-18 10:33:53 +0800

    优化注释

--- solon-ai-harness/src/main/java/org/noear/solon/ai/harness/agent/GenerateTool.java
+++ solon-ai-harness/src/main/java/org/noear/solon/ai/harness/agent/GenerateTool.java
@@ -56,6 +56,7 @@ public class GenerateTool extends AbsToolProvider {
 
 
     @ToolMapping(name = "generate_agent",
+            title = "generate",
             description = "动态创建一个具有特定能力和系统提示词的子代理\n" +
                     "- 优先使用 general 子代理（如果它不适合，才考虑创建新的子代理）\n" +
                     "- 创建前，可以先查阅任务相关的专家技能（如果有，可以更好的为子代理设计系统提示词）")
