2c5996eab0dbb380c959392701be73524e2bfd44
noear <noear@live.cn>
2026-04-21 21:44:43 +0800

    优化示例

--- solon-ai-agent/src/test/java/demo/ai/llm/LlmDemo.java
+++ solon-ai-agent/src/test/java/demo/ai/llm/LlmDemo.java
@@ -1,12 +1,23 @@
 package demo.ai.llm;
 
+import org.noear.solon.ai.agent.react.ReActAgent;
+
 /**
  *
  * @author noear 2026/1/4 created
  *
  */
 public class LlmDemo {
-    public static void main() {
+    public static void main() throws Throwable {
+        ReActAgent agent = ReActAgent.of(null)
+                .modelOptions(o -> {
 
+                })
+                .build();
+
+        agent.prompt("xxx").options(o -> {
+            //请求时动态切换 chatModel
+            o.chatModel(null);
+        }).call();
     }
 }
