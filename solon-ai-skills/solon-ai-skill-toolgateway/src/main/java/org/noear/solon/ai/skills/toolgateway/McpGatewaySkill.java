929ea0f4657ff915447ec8ae9090c7bbc3d8d5f8
SolonCode <soloncode@noear.org>
2026-05-29 14:12:55 +0800

    添加 McpGatewaySkill

---
+++ solon-ai-skills/solon-ai-skill-toolgateway/src/main/java/org/noear/solon/ai/skills/toolgateway/McpGatewaySkill.java
@@ -0,0 +1,20 @@
+package org.noear.solon.ai.skills.toolgateway;
+
+import org.noear.solon.ai.chat.skill.AbsSkill;
+import org.noear.solon.ai.mcp.client.McpClientProvider;
+import org.noear.solon.ai.mcp.client.McpServerParameters;
+
+/**
+ *
+ * @author noear 2026/5/29 created
+ *
+ */
+public class McpGatewaySkill extends AbsSkill {
+    public McpGatewaySkill addMcpServer(String name, McpClientProvider mcpProvider) {
+        return null;
+    }
+
+    public McpGatewaySkill addMcpServer(String name, McpServerParameters mcpParameters) {
+        return null;
+    }
+}
