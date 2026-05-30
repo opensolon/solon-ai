2f2c1623352a4fce2e3d1f6b0b881340530e8b7b
SolonCode <soloncode@noear.org>
2026-05-30 18:14:40 +0800

    添加 PoolDir 类并引入 alias 和 path 属性

---
+++ solon-ai-skills/solon-ai-skill-cli/src/main/java/org/noear/solon/ai/skills/cli/PoolDir.java
@@ -0,0 +1,13 @@
+package org.noear.solon.ai.skills.cli;
+
+import java.nio.file.Path;
+
+/**
+ *
+ * @author noear 2026/5/30 created
+ *
+ */
+public class PoolDir {
+    String alias;
+    String path;
+}
