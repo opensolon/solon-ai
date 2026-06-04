cb6d713f97ab277d655147dff1cc1e60d892e480
noear <noear@live.cn>
2026-06-04 22:48:47 +0800

    调整 solon-ai-harness VAR_MODEL_SELECTED 更名为 CTX_MODEL_SELECTED

--- solon-ai-harness/src/main/java/org/noear/solon/ai/harness/agent/TaskTalent.java
+++ solon-ai-harness/src/main/java/org/noear/solon/ai/harness/agent/TaskTalent.java
@@ -169,7 +169,7 @@ public class TaskTalent extends AbsTalent {
             LOG.debug("任务开始[{}/{} - {}]: {}", task.index, count, task.agent_name, ONode.serialize(task));
         }
 
-        String modelSelected = __parentSession.getContext().getAs(HarnessEngine.VAR_MODEL_SELECTED);
+        String modelSelected = __parentSession.getContext().getAs(HarnessEngine.CTX_MODEL_SELECTED);
 
 
         ReActAgent agent = agentDefinition.builder(engine, modelSelected).build();
